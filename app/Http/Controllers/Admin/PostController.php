369360dc1a740478c3ac7629510049675e0c59ed
Md.Aminul Islam Fahim <cipfahim1999@gmail.com>
2018-05-20 14:42:15 +0600

    post show doen

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -102,7 +102,7 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        //
+        return view('admin.post.show',compact('post'));
     }
 
     /**
--- resources/views/admin/post/index.blade.php
+++ resources/views/admin/post/index.blade.php
@@ -37,7 +37,7 @@
                                     <th>Is Approved</th>
                                     <th>Status</th>
                                     <th>Created At</th>
-                                    <th>Updated At</th>
+                                    {{--<th>Updated At</th>--}}
                                     <th>Action</th>
                                 </tr>
                                 </thead>
@@ -50,7 +50,7 @@
                                     <th>Is Approved</th>
                                     <th>Status</th>
                                     <th>Created At</th>
-                                    <th>Updated At</th>
+                                    {{--<th>Updated At</th>--}}
                                     <th>Action</th>
                                 </tr>
                                 </tfoot>
@@ -76,8 +76,11 @@
                                                 @endif
                                             </td>
                                             <td>{{ $post->created_at }}</td>
-                                            <td>{{ $post->updated_at }}</td>
+                                            {{--<td>{{ $post->updated_at }}</td>--}}
                                             <td class="text-center">
+                                                <a href="{{ route('admin.post.show',$post->id) }}" class="btn btn-info waves-effect">
+                                                    <i class="material-icons">visibility</i>
+                                                </a>
                                                 <a href="{{ route('admin.post.edit',$post->id) }}" class="btn btn-info waves-effect">
                                                     <i class="material-icons">edit</i>
                                                 </a>
