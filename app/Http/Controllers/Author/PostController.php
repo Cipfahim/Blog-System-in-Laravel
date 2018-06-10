9e6f34ab240d2809ae25d8caee997ccd3850b4c4
Md.Aminul Islam Fahim <cipfahim1999@gmail.com>
2018-06-10 15:46:51 +0600

    notify new author post

--- app/Http/Controllers/Author/PostController.php
+++ app/Http/Controllers/Author/PostController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers\Author;
 
 use App\Category;
+use App\Notifications\NewAuthorPost;
 use App\Tag;
 use App\Post;
+use App\User;
 use Brian2694\Toastr\Facades\Toastr;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Notification;
 use Illuminate\Support\Facades\Storage;
 use Intervention\Image\Facades\Image;
 
@@ -90,6 +93,8 @@ class PostController extends Controller
         $post->categories()->attach($request->categories);
         $post->tags()->attach($request->tags);
 
+        $users = User::where('role_id','1')->get();
+        Notification::send($users, new NewAuthorPost($post));
         Toastr::success('Post Successfully Saved :)','Success');
         return redirect()->route('author.post.index');
     }
---
+++ app/Notifications/NewAuthorPost.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class NewAuthorPost extends Notification
+{
+    use Queueable;
+
+    public $post;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($post)
+    {
+        $this->post = $post;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->greeting('Hello, Admin!')
+                    ->subject('New Post Approval Needed')
+                    ->line('New Post by '.$this->post->user->name . ' need to approve.')
+                    ->line('To approve the post click view button.')
+                    ->line('Post Title : '. $this->post->title)
+                    ->action('View', url(route('admin.post.show',$this->post->id)))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
