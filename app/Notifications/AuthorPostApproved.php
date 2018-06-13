8038a3ca140ce1775d06dc6980774a547e444be9
Md.Aminul Islam Fahim <cipfahim1999@gmail.com>
2018-06-13 14:36:32 +0600

    queue added

--- app/Notifications/AuthorPostApproved.php
+++ app/Notifications/AuthorPostApproved.php
@@ -7,7 +7,7 @@ use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 
-class AuthorPostApproved extends Notification
+class AuthorPostApproved extends Notification implements ShouldQueue
 {
     use Queueable;
     public $post;
--- app/Notifications/NewAuthorPost.php
+++ app/Notifications/NewAuthorPost.php
@@ -7,7 +7,7 @@ use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 
-class NewAuthorPost extends Notification
+class NewAuthorPost extends Notification implements ShouldQueue
 {
     use Queueable;
 
