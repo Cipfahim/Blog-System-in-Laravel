7d96283dfe0a5d0ca0f0a8dbaa17e57968944ace
Md. Aminul Islam Fahim <cipfahim1999@gmail.com>
2018-07-31 22:08:52 +0600

    author deshboard update

--- app/Http/Controllers/Author/DashboardController.php
+++ app/Http/Controllers/Author/DashboardController.php
@@ -4,11 +4,23 @@ namespace App\Http\Controllers\Author;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('author.dashboard');
+        $user = Auth::user();
+        $posts = $user->posts;
+        $popular_posts = $user->posts()
+            ->withCount('comments')
+            ->withCount('favorite_to_users')
+            ->orderBy('view_count','desc')
+            ->orderBy('comments_count')
+            ->orderBy('favorite_to_users_count')
+            ->take(5)->get();
+        $total_pending_posts = $posts->where('is_approved',false)->count();
+        $all_views = $posts->sum('view_count');
+        return view('author.dashboard',compact('posts','popular_posts','total_pending_posts','all_views'));
     }
 }
