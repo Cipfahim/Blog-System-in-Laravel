8dd992739003d4105c8739070f098fcc167cd374
Md. Aminul Islam Fahim <cipfahim1999@gmail.com>
2018-11-28 12:07:57 +0600

    database setup for heroku

--- config/database.php
+++ config/database.php
@@ -1,4 +1,9 @@
 <?php
+$url = parse_url(getenv('DATABASE_URL'));
+$host = $url["host"]??null;
+$username = $url["user"]??null;
+$password = $url["pass"]??null;
+$database = substr($url["path"],1)??null;
 
 return [
 
@@ -13,7 +18,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql_heroku'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,6 +70,17 @@ return [
             'prefix' => '',
             'schema' => 'public',
             'sslmode' => 'prefer',
+        ],
+
+        'pgsql_heroku' => [
+            'driver' => 'pgsql',
+            'host' => $host,
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
+            'charset' => 'utf8',
+            'prefix' => '',
+            'schema' => 'public',
         ],
 
         'sqlsrv' => [
